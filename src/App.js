34b62d4cf052431a4b03420da93048f31c439e9f
Akshay <akshay.sinha335@gmail.com>
2023-06-17 22:44:59 +0530

    product listing commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Signup from "./components/Signup"
 import Login from "./components/Login"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductListing from './components/ProductListing';
 
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
       <Routes>
         <Route path='/signup' element={<Signup />}/>
         <Route path='/login' element={<Login/>} />
+        <Route path='/ProductListing' element={<ProductListing/>} />
       </Routes>
     </Router>
+    
     </>
   );
 }
---
+++ src/components/ProductListing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/ProductListing.css'
+
+export default function ProductListing() {
+  return (
+    <>
+        <div>
+            <h1>Product List</h1>
+            <button className="btn">Add Products</button>
+        </div>
+    </>
+  )
+}
