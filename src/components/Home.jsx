266b0b3944bc17225ea81b16172d19552feadef6
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2023-06-17 22:05:37 +0530

    Fixed the Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,21 +6,24 @@ const Home = () => {
   return (
     <>
       <div className="first-div">
-        
+
         <div className="heading-div">
           <h2>Welcome To Fresh Farms!</h2>
           <h4>Let's Get You Started</h4>
         </div>
 
-        <form className="form-div">
-          <label htmlFor="name">Are you a Farmer or Customer?</label>
+        <div className="form-div">
+          {/* <label htmlFor="name">Are you a Farmer or Customer?</label> */}
+          <h5>Are you a Farmer or Customer?</h5>
           <br />
-          <input type="text" placeholder="Enter here" />
-        </form>
-
-        <div className="btn-div">
-          <Button color="dark">Lets Get Started!</Button>
+          {/* <input type="text" placeholder="Enter here" /> */}
+          <Button color="dark">Farmer</Button>
+          <Button color="dark">Customer</Button>
         </div>
+
+        {/* <div className="btn-div">
+          <Button color="dark">Lets Get Started!</Button>
+        </div> */}
       </div>
     </>
   );
