364b415bac572af37c709f838d10bfb6f5ed1255
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2023-06-19 00:18:33 +0530

    Login page fixed

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import "../components/styles/SignUp.css";
-// import { useNavigate } from "react-router-dom";
 
 
 const Signup = () => {
-  // const navigate = useNavigate();
-
-  // const navigateLogin = () => {
-  //   navigate("/login");
-  // };
-
   return (
     <>
       
